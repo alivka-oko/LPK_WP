500a7db73771560e73a2a29d011c7ac2a710eaf6
alivka-oko <alina.okotnikova2002@gmail.com>
2024-03-12 17:58:28 +0300

    Документы (не доделано)

--- wp-content/themes/forestgroup/documents.php
+++ wp-content/themes/forestgroup/documents.php
@@ -9,40 +9,36 @@ Template Name: Документы
         background-image: url(/assets/template/images/icon/icon-press-docs.svg);
     }
 </style>
-<section class="SecondBanner " style="background: url(/assets/template/images/banner/backgroundBanner7.jpg);">
+<section class="SecondBanner " style="background: url(<?= CFS()->get('background') ?>);">
     <div class="container">
         <div class="titleText">
-            <h1>Документы</h1>
+            <h1><?= CFS()->get('name') ?></h1>
         </div>
     </div>
 </section>
 <section class="content">
-    <div class="container smi-material__container mb-5">
-        <h2>Законодательные акты</h2>
-        <div class="row">
-            <div class="col-md-6 col-xl-4 smi-material__content mb-3">
-                <a class="smi-material__title" href="https://forest-groups.ru/assets/doc/privacy-policy.pdf" target="_blank">ПОЛИТИКА КОНФИДЕНЦИАЛЬНОСТИ</a>
-                <span class="smi-material__doc">PDF</span>
-                <span class="smi-material__value">0.32 МБ </span>
+    <?php
+    $sections = CFS()->get('sections');
+    foreach ($sections as $section) {
+    ?>
+        <div class="container smi-material__container mb-5">
+            <h2><?= $section['section_name'] ?></h2>
+            <div class="row">
+                <?php
+                $documents = $section['sections_documents'];
+                foreach ($documents as $document) {
+                ?>
+                    <div class="col-md-6 col-xl-4 smi-material__content mb-3">
+                        <a class="smi-material__title" href="<?= $document['document_file'] ?>" target="_blank"><?= $document['document_name'] ?></a>
+                        <span class="smi-material__doc">PDF</span>
+                        <span class="smi-material__value">0.32 МБ </span>
+                    </div>
+                <?php
+                } ?>
             </div>
         </div>
-    </div>
-    <div class="container smi-material__container">
-        <h2>Сертификаты</h2>
-        <div class="row">
-            <div class="col-md-6 col-xl-4 smi-material__content mb-3">
-                <a class="smi-material__title" href="https://forest-groups.ru/assets/doc/light-pellets.pdf" target="_blank">СЕРТИФИКАТ ИСПЫТАНИЙ</a>
-                <div class="description">Светлые пеллеты</div>
-                <span class="smi-material__doc">PDF</span>
-                <span class="smi-material__value">0.25 МБ</span>
-            </div>
-            <div class="col-md-6 col-xl-4 smi-material__content mb-3">
-                <a class="smi-material__title" href="https://forest-groups.ru/assets/doc/light-grey-pellets.pdf" target="_blank">СЕРТИФИКАТ ИСПЫТАНИЙ</a>
-                <div class="description">Светло-серые пеллеты</div>
-                <span class="smi-material__doc">PDF</span>
-                <span class="smi-material__value">0.28 МБ</span>
-            </div>
-        </div>
-    </div>
+    <?php
+    }
+    ?>
 </section>
 <?php get_footer() ?>
