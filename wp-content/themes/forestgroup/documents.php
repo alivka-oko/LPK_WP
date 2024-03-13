15af716191baa691e20e5d666df21fe184652a37
alivka-oko <alina.okotnikova2002@gmail.com>
2024-03-13 09:52:32 +0300

    Документы

--- wp-content/themes/forestgroup/documents.php
+++ wp-content/themes/forestgroup/documents.php
@@ -5,10 +5,17 @@ Template Name: Документы
 ?>
 <?php get_header() ?>
 <style>
-    .smi-material__content:before {
-        background-image: url(/assets/template/images/icon/icon-press-docs.svg);
+    <?php
+    $document_icons = CFS()->get('document_icons');
+    foreach ($document_icons as $document) {
+    ?>.smi-material__content.<?= $document['document_extension']; ?>:before {
+        background-image: url(<?= $document['document_ico']; ?>);
     }
+    <?php
+    }
+    ?>
 </style>
+
 <section class="SecondBanner " style="background: url(<?= CFS()->get('background') ?>);">
     <div class="container">
         <div class="titleText">
@@ -27,11 +34,23 @@ Template Name: Документы
                 <?php
                 $documents = $section['sections_documents'];
                 foreach ($documents as $document) {
+                    $url = $document['document_file'];
+                    $headers = get_headers($url, true);
+
+                    // Получение MIME-типа контента
+                    $contentType = isset($headers['Content-Type']) ? $headers['Content-Type'] : 'application/octet-stream';
+                    $extension = pathinfo($url, PATHINFO_EXTENSION);
+
+                    // Получение размера документа
+                    $contentLength = isset($headers['Content-Length']) ? $headers['Content-Length'] : 0;
+                    $sizeInMb = number_format($contentLength / (1024 * 1024), 2) . ' МБ';
+
                 ?>
                     <div class="col-md-6 col-xl-4 smi-material__content mb-3">
-                        <a class="smi-material__title" href="<?= $document['document_file'] ?>" target="_blank"><?= $document['document_name'] ?></a>
-                        <span class="smi-material__doc">PDF</span>
-                        <span class="smi-material__value">0.32 МБ </span>
+                        <a class="smi-material__title" href="<?= $url ?>" target="_blank"><?= $document['document_name'] ?></a>
+                        <div class="description"><?= $document['document_desc'] ?></div>
+                        <span class="smi-material__doc"><?= strtoupper($extension) ?></span>
+                        <span class="smi-material__value"><?= $sizeInMb ?></span>
                     </div>
                 <?php
                 } ?>
@@ -40,5 +59,15 @@ Template Name: Документы
     <?php
     }
     ?>
+
 </section>
+
+<script>
+    let documents = document.querySelectorAll('.smi-material__content');
+    for (let document_file of documents) {
+        let format = document_file.querySelector('.smi-material__doc');
+        document_file.classList.add(format.innerHTML)
+    }
+</script>
+
 <?php get_footer() ?>
